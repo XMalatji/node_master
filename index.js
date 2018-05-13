04b652d18d6a16e2886d569dadca832212389c52
Phaseka <phaseka.malatji@mmiholdings.co.za>
2018-05-13 09:19:04 +0200

    node crud

--- index.js
+++ index.js
@@ -1,7 +1,70 @@
 const http = require('http')
+const https = require('https')
 const url = require('url')
+const config = require('./config')
+const fs = require('fs');
 const { StringDecoder } = require('string_decoder')
-const server = http.createServer((req, res) => {
+var _data = require('./lib/data')
+
+
+// @TODO
+_data.delete('tests','nf4', function(err,data){
+  console.log('err', err, 'data',data)
+});
+const httpServer = http.createServer((req, res) => {
+    unifiedServer(req,res)
+})
+
+
+httpServer.listen(config.httpPort, () => {
+  console.log(`Server listening on port ${config.httpPort}`)
+})
+
+
+//instantiate https server
+
+let key = fs.readFileSync('./https/key.pem');
+let cert = fs.readFileSync('./https/cert.pem');
+
+
+
+var httpsOptions = {
+  'key': key,
+  'cert': cert
+};
+
+
+//start https
+var httpsServer = https.createServer(httpsOptions,function(req,res){
+  unifiedServer(req,res);
+});
+httpsServer.listen(config.httpsPort,function(){
+  console.log('The HTTPS server is running on port '+config.httpsPort);
+ });
+
+var handlers = {}
+handlers.ping = (data,callback) => {
+  callback(200);
+}
+handlers.advisors = (data, callback) => {
+  // callback http status code , and a payload object
+
+  callback(406, { 'handler name': 'advisor handler' })
+}
+
+handlers.notFound = (data, callback) => {
+  callback(404)
+}
+var router = {
+  advisors: handlers.advisors,
+  ping:handlers.ping
+}
+
+
+
+// unified server
+
+var unifiedServer = function(req,res){
   const parseUrl = url.parse(req.url, true)
   const path = parseUrl.pathname
   const trimmedPath = path.replace(/^\/+|\/+$/g, '')
@@ -23,14 +86,12 @@ const server = http.createServer((req, res) => {
 
   req.on('end', function () {
     buffer += decoder.end()
-  
 
     // choose handler which req should go to, if 404 use notfound
 
-
-    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ?
-     router[trimmedPath] : handlers.notFound;
-
+    var chosenHandler = typeof router[trimmedPath] !== 'undefined'
+      ? router[trimmedPath]
+      : handlers.notFound
 
     // construct data object for handler
 
@@ -43,7 +104,7 @@ const server = http.createServer((req, res) => {
     }
 
     // use rerqHandler for routing which contains route
-    chosenHandler(data,(statusCode,payload)=>{
+    chosenHandler(data, (statusCode, payload) => {
       // default status code
 
       statusCode = typeof statusCode === 'number' ? statusCode : 200
@@ -53,33 +114,15 @@ const server = http.createServer((req, res) => {
 
       // convert payload to string
 
-      var payloadString = JSON.stringify(payload);
-      //return response
+      var payloadString = JSON.stringify(payload)
+      // return response
+      res.setHeader('Content-Type', 'application/json')
+      res.writeHead(statusCode)
+      res.end(payloadString)
 
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      console.log('returniong this response',statusCode, payloadString )
+      console.log('returniong this response', statusCode, payloadString)
     })
-    
   })
-})
-
-var handlers = {}
-
-handlers.advisors = (data, callback) => {
-  // callback http status code , and a payload object
-
-  callback(406, { 'handler name': 'advisor handler' })
 }
 
-handlers.notFound = (data, callback) => {
-  callback(404);
-}
-var router = {
-  advisors: handlers.advisors
-}
 
-server.listen(6269, () => {
-  console.log(`Server listening on port 6269`)
-})
